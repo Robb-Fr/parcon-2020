690105b6752ccf104379174bbdb5c8b15465f0a1
Guillaume Martres <smarter@ubuntu.com>
2019-02-19 20:44:23 +0100

    Add scalashop assignment

---
+++ assignment.sbt
@@ -0,0 +1,9 @@
+// Student tasks (i.e. submit, packageSubmission)
+enablePlugins(StudentTasks)
+
+courseraId := ch.epfl.lamp.CourseraId(
+  key = "OpSNmtC1EeWvXAr2bF16EQ",
+  itemId = "MhXvy",
+  premiumItemId = Some("NeGTv"),
+  partId = "Q2e1P"
+)
---
+++ src/test/scala/scalashop/BlurSuite.scala
@@ -0,0 +1,11 @@
+package scalashop
+
+import java.util.concurrent._
+import scala.collection._
+import org.junit._
+import org.junit.Assert.assertEquals
+
+class BlurSuite {
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
+}
