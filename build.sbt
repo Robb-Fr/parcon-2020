0a94b65038a0a6bc9a919afeae35d0bc651995b3
Guillaume Martres <smarter@ubuntu.com>
2019-02-19 20:44:23 +0100

    Add stackoverflow assignment

---
+++ build.sbt
@@ -0,0 +1,20 @@
+course := "bigdata"
+assignment := "stackoverflow"
+
+scalaVersion := "0.24.0-RC1"
+scalacOptions ++= Seq("-language:implicitConversions", "-deprecation")
+libraryDependencies ++= Seq(
+  "com.novocode" % "junit-interface" % "0.11" % Test,
+  ("org.apache.spark" %% "spark-core" % "3.0.0-X1").withDottyCompat(scalaVersion.value),
+)
+
+// Contains Spark 3 snapshot built against 2.13: https://github.com/smarter/spark/tree/scala-2.13
+resolvers += Resolver.bintrayRepo("smarter", "maven")
+
+testOptions in Test += Tests.Argument(TestFrameworks.JUnit, "-a", "-v", "-s")
+
+testSuite := "stackoverflow.StackOverflowSuite"
+
+// Without forking, ctrl-c doesn't actually fully stop Spark
+fork in run := true
+fork in Test := true
---
+++ src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -0,0 +1,47 @@
+package stackoverflow
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import org.junit._
+import org.junit.Assert.assertEquals
+import java.io.File
+import scala.io.{ Codec, Source }
+import scala.util.Properties.isWin
+
+object StackOverflowSuite {
+  if (isWin) System.setProperty("hadoop.home.dir", System.getProperty("user.dir") + "\\winutils\\hadoop-2.7.4")
+
+  val conf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("StackOverflow")
+  val sc: SparkContext = new SparkContext(conf)
+}
+
+class StackOverflowSuite {
+  import StackOverflowSuite._
+
+
+  lazy val testObject = new StackOverflow {
+    override val langs =
+      List(
+        "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+        "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+    override def langSpread = 50000
+    override def kmeansKernels = 45
+    override def kmeansEta: Double = 20.0D
+    override def kmeansMaxIterations = 120
+  }
+
+  @Test def `testObject can be instantiated`: Unit = {
+    val instantiatable = try {
+      testObject
+      true
+    } catch {
+      case _: Throwable => false
+    }
+    assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(300 * 1000)
+}
