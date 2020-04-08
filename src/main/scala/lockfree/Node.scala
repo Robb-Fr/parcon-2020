14b754da2c4af22b47843991c661878ed8e8375c
tdemont <tom.demont@epfl.ch>
2020-04-08 16:38:41 +0200

    run grading tests

--- src/main/scala/lockfree/Node.scala
+++ src/main/scala/lockfree/Node.scala
@@ -3,20 +3,23 @@ package lockfree
 abstract class Node(val value: Int, initTail: Option[Node]) {
 
   // The type of mutable state held by node.
-  type State = Option[Node]
+  type State = (Option[Node], Boolean)
 
   // The initial mutable state of the node.
-  def initialState: State = initTail
+  def initialState: State = (initTail, false)
 
   // The atomic variable that holds the state.
   val atomicState: AbstractAtomicVariable[State] = new AtomicVariable[State](initialState)
 
   // Function to read the next node from the state.
-  def next: Option[Node] = atomicState.get
+  def next: Option[Node] = atomicState.get._1
 
   // Should return true if the node was marked as deleted.
-  def deleted: Boolean = false
+  def deleted: Boolean = atomicState.get._2
 
   // Should mark the node as deleted.
-  def mark: Boolean = ???
+  def mark: Boolean = {
+    val state = atomicState.get
+    atomicState.compareAndSet((state._1, false), (state._1, true))
+  }
 }
--- src/main/scala/lockfree/SortedList.scala
+++ src/main/scala/lockfree/SortedList.scala
@@ -12,15 +12,41 @@ class SortedList extends AbstractSortedList {
 
   // Finds the first node whose value satisfies the predicate.
   // Returns the predecessor of the node and the node.
-  def findNodeWithPrev(pred: Int => Boolean): (Node, Option[Node]) = ???
+  def findNodeWithPrev(pred: Int => Boolean): (Node, Option[Node]) = 
+    def rec(predecessor: Node, current: Option[Node]): (Node, Option[Node]) =
+      current match
+        case None => (predecessor, None)
+        case Some(n) => 
+          n.atomicState.get match
+            case (next, false) => if (pred(n.value)) (predecessor, current) else rec(n, n.next)
+            case (next, true) => predecessor.atomicState.compareAndSet((current, false), (next, false))
+              findNodeWithPrev(pred)
+
+    rec(_head, firstNode)
 
   // Insert an element in the list.
-  def insert(e: Int): Unit = ???
+  def insert(e: Int): Unit = 
+    val (prev, node) = findNodeWithPrev(n => n >= e)
+    val newNode = (prev, node) match
+      case (x, None) => createNode(e, None)
+      case (a, Some(b)) => createNode(e, Some(b))
+    
+    if(!prev.atomicState.compareAndSet((node, false), (Some(newNode), false)))
+      insert(e)
 
   // Checks if the list contains an element.
-  def contains(e: Int): Boolean = ???
+  def contains(e: Int): Boolean = findNodeWithPrev(n => n == e) match
+    case (_, None) => false
+    case (_, Some(_)) => true
 
   // Delete an element from the list.
   // Should only delete one element when multiple occurences are present.
-  def delete(e: Int): Boolean = ???
+  def delete(e: Int): Boolean = 
+    val toDelete = findNodeWithPrev(n => n == e)
+    val deleted = toDelete match
+      case (n, None) => return false
+      case (p, Some(c)) => c.mark
+   
+    if(!deleted) delete(e)
+    else deleted
 }
