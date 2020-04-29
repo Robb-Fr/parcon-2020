f7d04d3dda43743504f95c1b3a9734141a9a658d
tdemont <tom.demont@epfl.ch>
2020-04-29 17:37:02 +0200

    runGradingTests

--- src/main/scala/actorbintree/BinaryTreeSet.scala
+++ src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -58,6 +58,8 @@ class BinaryTreeSet extends Actor {
 
   var root = createRoot
 
+  override def toString = "BinaryTreeSet of root "+root
+
   // optional (used to stash incoming operations during garbage collection)
   var pendingQueue = Queue.empty[Operation]
 
@@ -66,14 +68,28 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case op: Operation => root!(op)
+    case GC => 
+      val newRoot = createRoot
+      root!CopyTo(newRoot)
+      context.become(garbageCollecting(newRoot))
+    case others => println("Unknown message received : "+others+" by "+this) }
 
   // optional
   /** Handles messages while garbage collection is performed.
     * `newRoot` is the root of the new binary tree where we want to copy
     * all non-removed elements into.
     */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
+  def garbageCollecting(newRoot: ActorRef): Receive = 
+    case op: Operation => pendingQueue = pendingQueue.appended(op)
+    case CopyFinished =>
+      root = newRoot
+      context.become(normal)
+      pendingQueue.foreach(op => self!op)
+      pendingQueue = Queue.empty[Operation]
+    case GC => 
+    case others => println("Unknown message received : "+others+" by "+this)
 
 }
 
@@ -101,18 +117,81 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
   var subtrees = Map[Position, ActorRef]()
   var removed = initiallyRemoved
 
+  override def toString = "BinaryTreeNode of elem "+elem
+
   // optional
   def receive = normal
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case op: Operation => 
+      val rightSub = if(elem < this.elem) Left else Right
+      op match
+        case Insert(requester, id, elem) => 
+          if(elem == this.elem) 
+            removed = false 
+            requester!(OperationFinished(id))
+          else 
+            subtrees.get(rightSub) match
+              case None => 
+                subtrees = subtrees+(rightSub -> context.actorOf(props(elem, false)))
+                requester!(OperationFinished(id))
+              case Some(t) => t!(op)
+
+        case Contains(requester, id, elem) => 
+          if(elem == this.elem && removed == false)
+            requester!(ContainsResult(id, true))
+          else
+            subtrees.get(rightSub) match
+              case None => requester!(ContainsResult(id, false))
+              case Some(t) => t!(op)
+
+        case Remove(requester, id, elem) => 
+          if(elem == this.elem) 
+            removed = true 
+            requester!(OperationFinished(id))
+          else 
+            subtrees.get(rightSub) match
+              case None => requester!(OperationFinished(id))
+              case Some(t) => t!(op)
+
+    case CopyTo(treeNode) => 
+      var expected: Set[ActorRef] = Set.empty
+
+      if(!removed)
+        treeNode!Insert(self, this.elem, this.elem)
+
+      subtrees.get(Left) match
+        case Some(a) => expected+=a
+          a!CopyTo(treeNode)
+        case _ =>
+
+      subtrees.get(Right) match
+        case Some(a) => expected+=a
+          a!CopyTo(treeNode)
+        case _ =>
+
+      if(expected.isEmpty && removed)
+        context.parent!CopyFinished
+        self!PoisonPill
+      else
+        context.become(copying(expected, removed))
+
+    case others => println("Unknown message received : "+others+" by "+this) }
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
-
-
+  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = 
+    case OperationFinished(_) => context.become(copying(expected, true))
+      if((expected).isEmpty)
+        context.parent!CopyFinished
+        self!PoisonPill
+    case CopyFinished => context.become(copying(expected-sender, insertConfirmed))
+      if((expected-sender).isEmpty && insertConfirmed)
+        context.parent!CopyFinished
+        self!PoisonPill
+    case others => println("Unknown message received : "+others+" by "+this)
 }
